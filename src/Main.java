ca71e69b4242c4bc64851ee78f429b0795c9d650
ppanosyan <pedro.panosyan@ing.autral.edu.ar>
2022-11-01 13:15:48 -0300

    /git. commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public void main(){
-        int i  = 0;
         int h = 2;
+        int o = 1;
+        for (int i = 0; i < 5; i++){
+            System.out.println("");
+        }
+
     }
 }
+
